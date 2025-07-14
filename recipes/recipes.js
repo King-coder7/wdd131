56843bb655ef6049f9558cb3805e52cc2dc2e28e
yourusername <San22039@byui.edu>
2025-07-14 01:17:25 -0600

    recipe

---
+++ recipes/recipes.js
@@ -0,0 +1,11 @@
+// Recipe data (example)
+const recipe = {
+    title: "Apple Crisp",
+    tags: ["dessert"],
+    image: "images/apple-crisp.jpg",
+    rating: 4,
+    description: "This apple crisp recipe is a simple yet delicious fall dessert that's great served warm with vanilla ice cream."
+};
+
+// Future JavaScript for dynamic loading or interactivity would go here
+console.log("Recipes loaded");
